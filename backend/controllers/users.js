4cb375bc3349ed0bcc91f54ce9d08aaa73286c22
AshourAlaa <ashour.alaaj@gmail.com>
2022-11-03 19:51:41 +0200

    register

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,4 @@
-const pool = require("../models/db");
+const {pool} = require("../models/db");
 const bcrypt = require("bcrypt");
 const saltRounds = parseInt(process.env.SALT);
  
@@ -16,5 +16,22 @@ const register =async (req,res)=>{
       ];
 
     const query=`INSERT INTO users (firstName, lastName, age, country, email, password, role_id) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
-    pool.query(query,values).then().catch()
+    pool.query(query,values).then((result) => {
+        res.status(200).json({
+          success: true,
+          massage: "Account Created Successfully",
+        });
+      })
+      .catch((err) => {
+        res.status(409).json({
+          success: false,
+          massage: "The email already exists",
+          err,
+        });
+      })
 }
+
+module.exports = {
+    register,
+  };
+  
