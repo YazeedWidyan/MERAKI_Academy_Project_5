1f8011c4c9afa04dfb5118ea4a41324e8ed1f0f7
YazeedWidyan <yazeedwidyany@gmail.com>
2022-11-04 20:40:58 +0300

    complete checkout

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Template from "../containers/Template";
 import ProductDetails from "../containers/ProductDetails";
+import Checkout from "../containers/Checkout";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/template" element={<Template />} />
         <Route path="/productDetails" element={<ProductDetails />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<p>There's nothing here: 404!</p>} />
       </Routes>
     </>
