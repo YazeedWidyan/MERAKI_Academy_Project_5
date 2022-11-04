bc328f58c15d8514ab2e8f75ae315e174c22f797
OmarAlduais <omar.a.alduais@gmail.com>
2022-11-04 09:55:07 +0200

    routes create role, permission, role_permission

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const {createNewRole, createNewPermission, createNewRolePermission} = require('../controllers/role')
 
 const roleRouter = express.Router();
 
-roleRouter.post("/");
+roleRouter.post("/", createNewRole);
+roleRouter.post("/permission", createNewPermission);
+roleRouter.post("/role_permission", createNewRolePermission);
 roleRouter.get("/");
 roleRouter.put("/");
 roleRouter.delete("/");
