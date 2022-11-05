bcfb77173732e2eeaa17a62e1f595dd4c493ede7
AshourAlaa <ashour.alaaj@gmail.com>
2022-11-05 13:28:18 +0200

    2nd try

---
+++ backend/controllers/login.js
@@ -0,0 +1,46 @@
+const {pool} =require("../models/db")
+const bcrypt =require("bcrypt")
+const jwt =require("jsonwebtoken")
+
+const login =(req,res)=>{
+ const password =req.body.password;
+ const email=req.body.email;
+ const values =[email.toLowerCase()];
+ const query=`SELECT * FROM users WHERE email = $1` ; 
+ pool
+ .query(query, values)
+ .then((result) => {
+   if (result.rows.length) {
+     bcrypt.compare(password, result.rows[0].password, (err, response) => {
+       if (err) res.json(err);
+       if (response) {
+         const payload = {
+           userId: result.rows[0].id,
+           country: result.rows[0].country,
+           role: result.rows[0].role_id,
+         };
+         const options = { expiresIn: "1d" };
+         const secret = process.env.SECRET;
+         const token = jwt.sign(payload, secret, options);
+         if (token) {
+           return res.status(200).json({ token, userId: result.rows[0].id });
+         } else {
+           throw Error;
+         }
+       } else {
+         res.status(403).json({
+           success: false,
+           message: `The password you’ve entered is incorrect`,
+         });
+       }
+     });
+   } else throw Error;
+ })
+ .catch((err) => {
+   res
+     .status(404)
+     .json({ success: false, message: "The email doesn't exist", err });
+ }); 
+}
+
+module.exports={login}
---
+++ backend/controllers/product.js
@@ -0,0 +1,5 @@
+const pool = require("../models/db");
+
+const getAllProducts=(req,res)=>{
+
+}
