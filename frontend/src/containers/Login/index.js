c12082b6036c7351cf1992c6ea24a8e25ac8104c
AshourAlaa <ashour.alaaj@gmail.com>
2022-11-06 21:33:25 +0200

    component

--- frontend/src/containers/Login/index.js
+++ frontend/src/containers/Login/index.js
@@ -1,7 +1,41 @@
-import React from "react";
 
+import "./login.style.css";
+import axios from "axios";
+import React, {  useState, useEffect } from "react";
+import { useDispatch ,useSelector } from "react-redux";
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+const login=()=>{
+  axios.post("http://localhost:5000/login", {
+    email,
+    password,
+  }).then().catch()
+}
+  return (<>
+  <div >
+    <p>login</p>
+    <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            
+          />
+              <button
+            onClick={(e) => {
+             
+            }}
+          >
+            Login
+          </button>
+  </div>
+  </>)
 };
 
 export default Login;
