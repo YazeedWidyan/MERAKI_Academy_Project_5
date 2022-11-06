43f079f97d93eb7490791baa58a5c2b3b0b49373
OmarAlduais <omar.a.alduais@gmail.com>
2022-11-06 23:41:25 +0200

    home categories

--- frontend/src/containers/Home/index.js
+++ frontend/src/containers/Home/index.js
@@ -1,22 +1,101 @@
 import axios from "axios";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./home.style.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getMenProducts,
+  getWomenProducts,
+  getKidsProducts
+} from "../../redux/selectors/products.selectors";
+import {
+  setMenProducts,
+  setWomenProducts,
+  setKidsProducts
+} from "../../redux/reducers/products";
 const Home = () => {
+  // const [products, setProducts] = useState([]);
 
-  const getMenItems =  () =>{
-   axios.get(`http://localhost:5000/product/catgory/1`)
-   .then((data)=>{
-    console.log(data);
-   })
-   .catch((err)=>{
-    console.log(err);
-   })
+  const menProducts = useSelector(getMenProducts);
+  const womenProducts = useSelector(getWomenProducts);
+  const kidsProducts = useSelector(getKidsProducts)
+  const dispatch = useDispatch();
+
+  const getMenItems = () => {
+    axios
+      .get(`http://localhost:5000/product/catgory/1`)
+      .then((data) => {
+        dispatch(setMenProducts(data.data.result));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getWomenItems = () => {
+    axios
+      .get(`http://localhost:5000/product/catgory/2`)
+      .then((data) => {
+        dispatch(setWomenProducts(data.data.result));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getKidsItems = () => {
+    axios
+      .get(`http://localhost:5000/product/catgory/3`)
+      .then((data)=>{
+dispatch(setKidsProducts(data.data.result))
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
   }
 
   useEffect(() => {
-   getMenItems()
+    getMenItems();
+    getWomenItems();
+    getKidsItems()
   }, []);
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <div>
+        {menProducts.map((product, i) => {
+          return (
+            <div key={i}>
+              <h3>{product.category_id}</h3>
+              <h4>{product.title}</h4>
+              <h5>{product.img}</h5>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        {womenProducts.map((product, i) => {
+          return (
+            <div key={i}>
+              <h3>{product.category_id}</h3>
+              <h4>{product.title}</h4>
+              <h5>{product.img}</h5>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        {kidsProducts.map((product, i) => {
+          return (
+            <div key={i}>
+              <h3>{product.category_id}</h3>
+              <h4>{product.title}</h4>
+              <h5>{product.img}</h5>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
