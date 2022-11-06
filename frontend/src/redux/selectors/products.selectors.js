a1817335417d51a680f6f7396e06a1e3d6671eae
OmarAlduais <omar.a.alduais@gmail.com>
2022-11-06 23:44:03 +0200

    selector categories

--- frontend/src/redux/selectors/products.selectors.js
+++ frontend/src/redux/selectors/products.selectors.js
@@ -1,1 +1,4 @@
 export const gettAllProducts = (state) => state.products.products;
+export const getMenProducts = (state) => state.products.menProducts;
+export const getWomenProducts = (state) => state.products.womenProducts;
+export const getKidsProducts = (state) => state.products.kidsProducts;
