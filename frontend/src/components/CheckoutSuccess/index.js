757a56228c14e337cb050f4597283bd164099bcc
YazeedWidyan <yazeedwidyany@gmail.com>
2022-11-06 21:26:36 +0300

    complete checkout success page

--- frontend/src/components/CheckoutSuccess/index.js
+++ frontend/src/components/CheckoutSuccess/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./checkoutsuccess.style.css";
+console.log("yazeed");
 const CheckoutSuccess = () => {
-  return <div>CheckoutSuccess</div>;
+  return (
+    <div className="success-checkout-container">
+      <div className="success-checkout-wrapper">
+        <img
+          className="success-checkout-image"
+          src="./assets/images/check.png"
+          alt="checkout success"
+        />
+        <h1>THANK YOU FOR YOUR PURCHASE</h1>
+        <div className="success-checkout-btn">Continue Shopping</div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutSuccess;
