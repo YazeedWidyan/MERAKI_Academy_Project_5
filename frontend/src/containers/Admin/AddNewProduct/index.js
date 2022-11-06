022f2934159c54ee41cbf4b64036443e45204f2b
AshourAlaa <ashour.alaaj@gmail.com>
2022-11-06 22:39:36 +0200

    add func

--- frontend/src/containers/Admin/AddNewProduct/index.js
+++ frontend/src/containers/Admin/AddNewProduct/index.js
@@ -1,8 +1,35 @@
 import React from "react";
 import "./addnewproduct.style.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 
 const AddNewProduct = () => {
-  return <div>addnewproduct</div>;
+  const [title, settitle] = useState('')
+  const [descriptions, setdescriptions] = useState('')
+  const [category_id, setcategory_id] = useState(0)
+  const [img, setimg] = useState('')
+  const [price,setprice]=useState(0)
+  const addProduct=()=>{
+    axios.post(`http://localhost:5000/product/add`,{
+      title,
+      descriptions,
+      category_id,
+      img,
+      price
+    }).then((result)=>{
+      console.log(result.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+  return <>
+  <div>
+    <p>new product</p>
+    <br/>
+    <input type={"text"}
+  </div>
+  </>;
 };
 
 export default AddNewProduct;
