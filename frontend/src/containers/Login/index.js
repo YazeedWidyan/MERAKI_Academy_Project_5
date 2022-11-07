404eb83eb89548ff5d7608c5a122b8b4cdc5eef7
AshourAlaa <ashour.alaaj@gmail.com>
2022-11-07 21:54:35 +0200

    login with google

--- frontend/src/containers/Login/index.js
+++ frontend/src/containers/Login/index.js
@@ -5,48 +5,46 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setLogin, setUserId, setUserType } from "../../redux/reducers/auth";
 import { getIsLoggedIn } from "../../redux/selectors/auth.selectors";
-import  GoogleLogin  from "react-google-login";
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
-/*
-to add google login 
-first install npm i react-google-login
-2nd 
-import { GoogleLogin } from "react-google-login";
-3rd
-declare clientId="1078773011571-4b9avs3roskdsl5oo7k5s9pt7o7tta8m.apps.googleusercontent.com"
- */
+
+
+
+
 const Login = () => {
-  const [loginData, setLoginData] = useState(
-    localStorage.getItem('loginData')
-      ? JSON.parse(localStorage.getItem('loginData'))
-      : null
-  );
-  const clientId="646674207004-f5s33oa3mbvsq5rnhthd67bnmjj439pg.apps.googleusercontent.com"
-  const onSuccess=(res)=>{
-    console.log('login success currentUser:',res.profileObj);
 
-  }
-  const onFailure =(err)=>{
-    console.log('login failed res:',err);
-  }
-  const handleLogin = async (googleData) => {
-    const res = await fetch('/api/google-login', {
-      method: 'POST',
-      body: JSON.stringify({
-        token: googleData.tokenId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await res.json();
-    setLoginData(data);
-    localStorage.setItem('loginData', JSON.stringify(data));
-  };
-  const handleLogout = () => {
-    localStorage.removeItem('loginData');
-    setLoginData(null);
-  };
+  const clientId="646674207004-f5s33oa3mbvsq5rnhthd67bnmjj439pg.apps.googleusercontent.com"
+ 
+    useEffect(() => {
+      function start() {
+        gapi.client.init({
+          clientId: clientId,
+          scope: 'email',
+        });
+      }
+  
+      gapi.load('client:auth2', start);
+    }, []);
+  
+    const onSuccess = response => {
+      console.log('SUCCESS', response);
+      console.log(response.googleId);
+      dispatch(setLogin(response.tokenId));
+      dispatch(setUserId(response.googleId));
+      dispatch(setUserType(1));
+   
+        navigate("/");
+ 
+
+    };
+    const onFailure = response => {
+      console.log('FAILED', response);
+    };
+    const onLogoutSuccess = () => {
+      console.log('SUCESS LOG OUT');
+    };
+  
   const state = useSelector(getIsLoggedIn);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -60,7 +58,7 @@ const Login = () => {
       })
       .then((result) => {
         console.log(result);
-        console.log(result);
+   
         dispatch(setLogin(result.data.token));
         dispatch(setUserId(result.data.userId));
         dispatch(setUserType(result.data.role));
@@ -97,20 +95,18 @@ const Login = () => {
         >
           Login
         </button>
-        {loginData ? (
-            <div>
-              <h3>You logged in as {loginData.email}</h3>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-          ) : (
-            <GoogleLogin
-              clientId={clientId}
-              buttonText="Log in with Google"
-              onSuccess={handleLogin}
-              onFailure={onFailure}
-              cookiePolicy={'single_host_origin'}
-            ></GoogleLogin>
-          )}
+        <div>
+      <GoogleLogin
+        clientId={clientId}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+      />
+      <GoogleLogout
+        clientId={clientId}
+        onLogoutSuccess={onLogoutSuccess}
+      />
+    </div>
+        
       </div>
     </>
   );
