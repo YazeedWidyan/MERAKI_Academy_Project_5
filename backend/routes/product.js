dfcf98a21a6e9fc6943787f96afcafe9bdb5bfc2
YazeedWidyan <yazeedwidyany@gmail.com>
2022-11-07 22:00:49 +0300

    update backend

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -14,8 +14,8 @@ const productRouter = express.Router();
 productRouter.post("/add", addProduct);
 productRouter.get("/", getAllProducts);
 productRouter.get("/:id", getProductById);
-productRouter.put("/", updateProducts);
-productRouter.delete("/delete", deleteProduct);
+productRouter.put("/update/:id", updateProducts);
+productRouter.delete("/delete/:id", deleteProduct);
 productRouter.get("/search/product", searchProduct);
 productRouter.get("/catgory/:category_id", getProductByCatagory);
 
