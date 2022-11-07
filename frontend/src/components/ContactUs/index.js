d0c7fd9ad31685feaa83d4319f7c0a097e35a874
OmarAlduais <omar.a.alduais@gmail.com>
2022-11-07 22:59:47 +0200

    contact us

--- frontend/src/components/ContactUs/index.js
+++ frontend/src/components/ContactUs/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser'
 import "./contactus.style.css";
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  const form = useRef()
+  const sendEmail = (e) =>{
+    e.preventDefault();
+    emailjs.sendForm('service_54e8mtn', 'template_t3clcq6', form.current, '767wuq3MKo9vm3GPF')
+    .then((result)=>{
+console.log(result);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  return <div className="contact-container">
+    <form className="contact-us" ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  </div>;
 };
 
 export default ContactUs;
